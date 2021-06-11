dde81226ca09b436d28db12b479fc48c4673e568
geokats <geo.katsogiannis@gmail.com>
2021-06-11 15:50:48 +0300

    First working version of conversion script for unified dataset

--- proc/scripts/data/unified/convert_examples.py
+++ proc/scripts/data/unified/convert_examples.py
@@ -4,30 +4,54 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("file", type=str)
+    parser.add_argument("input_file", type=str)
+    parser.add_argument("output_file", type=str)
     args = parser.parse_args()
 
-    print(f"Loading from {args.file}...")
+    print(f"Converting examples from {args.input_file} to {args.output_file}...")
 
-    with open(args.file, 'r') as f:
-        examples = json.load(f)
+    with open(args.input_file, 'r') as inpf, open(args.output_file, 'w') as outf:
+        examples = json.load(inpf)
         print(f"Found {len(examples)} examples")
 
         for i, example in enumerate(examples):
-            print(f"{15*'='} Example {i} {15*'='}")
-
             sentence = example['words']
+            ner = []
+            relation = []
+            event = []
 
-            for entity in example['golden-entity-mentions']:
-                entity_type = entity['entity-type']
+            #Get NER information
+            for example_entity in example['golden-entity-mentions']:
+                entity_start = example_entity['start']
+                entity_end = example_entity['end'] - 1
+                entity_type = example_entity['entity-type']
 
-            for event in example['golden-event-mentions']:
-                event_type = event['event-type']
-                trigger = event['trigger']
-                arguments = event['arguments']
+                ner.append([entity_start, entity_end, entity_type])
 
-                print(f"Event type: {event_type}")
-                print(f"Trigger: {trigger}")
-                print(f"Arguments: {arguments}")
+            #Get event trigger and arguments
+            for example_event in example['golden-event-mentions']:
+                event_type = example_event['event-type']
+                trigger = example_event['trigger']
+                arguments = example_event['arguments']
 
-            print(f"{45*'='}\n")
+                if trigger['end'] - trigger['start'] != 1:
+                    print(f"WARNING: Trigger \"{trigger['text']}\" is longer than one word! Only first word will be kept.")
+
+                converted_event = [[trigger['start'], event_type]]
+
+                for argument in arguments:
+                    converted_event.append([argument['start'], argument['end'], argument['role']])
+
+                event.append(converted_event)
+
+            #Create converted example
+            converted_example = {
+                'sentence' : sentence,
+                's_start' : 0,
+                'ner' : ner,
+                'relation' : relation,
+                'event' : event
+            }
+            #Write to output file
+            json.dump(converted_example, outf)
+            outf.write("\n")
