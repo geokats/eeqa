10f24effbb92c0a97c83c038224c3f6747257733
geokats <geo.katsogiannis@gmail.com>
2021-06-29 13:47:27 +0300

    Change script for jsonlines format

--- proc/scripts/data/unified/convert_examples.py
+++ proc/scripts/data/unified/convert_examples.py
@@ -1,6 +1,46 @@
 import json
 import argparse
 
+def convert_example(example):
+    sentence = example['words']
+    ner = []
+    relation = []
+    event = []
+
+    #Get NER information
+    for example_entity in example['golden-entity-mentions']:
+        entity_start = example_entity['start']
+        entity_end = example_entity['end'] - 1
+        entity_type = example_entity['entity-type']
+
+        ner.append([entity_start, entity_end, entity_type])
+
+    #Get event trigger and arguments
+    for example_event in example['golden-event-mentions']:
+        event_type = example_event['event-type']
+        trigger = example_event['trigger']
+        arguments = example_event['arguments']
+
+        if trigger['end'] - trigger['start'] != 1:
+            print(f"WARNING: Trigger \"{trigger['text']}\" is longer than one word! Only first word will be kept.")
+
+        converted_event = [[trigger['start'], event_type]]
+
+        for argument in arguments:
+            converted_event.append([argument['start'], argument['end'], argument['role']])
+
+        event.append(converted_event)
+
+    #Create converted example
+    converted_example = {
+        'sentence' : sentence,
+        's_start' : 0,
+        'ner' : ner,
+        'relation' : relation,
+        'event' : event
+    }
+
+    return converted_example
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -11,47 +51,13 @@ if __name__ == '__main__':
     print(f"Converting examples from {args.input_file} to {args.output_file}...")
 
     with open(args.input_file, 'r') as inpf, open(args.output_file, 'w') as outf:
-        examples = json.load(inpf)
-        print(f"Found {len(examples)} examples")
-
-        for i, example in enumerate(examples):
-            sentence = example['words']
-            ner = []
-            relation = []
-            event = []
-
-            #Get NER information
-            for example_entity in example['golden-entity-mentions']:
-                entity_start = example_entity['start']
-                entity_end = example_entity['end'] - 1
-                entity_type = example_entity['entity-type']
-
-                ner.append([entity_start, entity_end, entity_type])
-
-            #Get event trigger and arguments
-            for example_event in example['golden-event-mentions']:
-                event_type = example_event['event-type']
-                trigger = example_event['trigger']
-                arguments = example_event['arguments']
-
-                if trigger['end'] - trigger['start'] != 1:
-                    print(f"WARNING: Trigger \"{trigger['text']}\" is longer than one word! Only first word will be kept.")
-
-                converted_event = [[trigger['start'], event_type]]
-
-                for argument in arguments:
-                    converted_event.append([argument['start'], argument['end'], argument['role']])
-
-                event.append(converted_event)
-
-            #Create converted example
-            converted_example = {
-                'sentence' : sentence,
-                's_start' : 0,
-                'ner' : ner,
-                'relation' : relation,
-                'event' : event
-            }
+        count = 0
+        for line in inpf:
+            example = json.loads(line)
+            converted_example = convert_example(example)
             #Write to output file
             json.dump(converted_example, outf)
             outf.write("\n")
+            count += 1
+
+    print(f"Converted {count} examples")
